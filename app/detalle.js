728a0bf755fcad0ece2ff6e7a6a96de3483a94be
orttegato <ireneteotni@gmail.com>
2024-01-12 12:20:23 +0100

    first commit

---
+++ app/detalle.js
@@ -0,0 +1,83 @@
+window.onload = () => {
+    let lista = document.querySelector("#usuarios");
+    let obtenerParam = (url) => {
+        let urlParam = String(url.match(/\?+.+/));
+        urlParam = urlParam.replace("?id=", "");
+        return urlParam;
+    }
+
+    let param = obtenerParam(document.URL);
+
+    fetch('assets/data/anuario.json')
+        .then(res => res.json())
+        .then(data => {
+            let project = data.filter((proj) => {
+                return proj.Id == param;
+            });
+            let images =  [ project[0].imagenes.slice(0,62), project[0].imagenes.slice(64,126)]
+
+                let estructura = `
+                    <header>
+                        <ul class="menu-header-detalle">
+                        <li class="el-menu-header-a"><a href="../index.html">About</a></li>
+                            <li class="el-menu-header">More</li>
+                        </ul>
+                    </header>
+                    <section class="contenido">
+                        <div class="contenido-container">
+                            <div class="contenido-left">
+                                <img src="${images[0]}" />
+                            </div>
+                            <div class="contenido-right">
+                                <div class="contenido-right-container">
+                                    <div class="contenido-titulo">
+                                        <h1>${project[0].titulo}</h1>
+                                        <p class="autor-contenido">${project[0].nombre_estudiante}</p>
+                                    </div>
+
+                                    <div class="contenido-info">
+                                        <div class="contenido-info-u">
+                                            <p>Alumno</p>
+                                            <p>${project[0].nombre_estudiante}</p>
+                                            <p>${project[0].correo_estudiante}</p>
+                                            <p><a href="${project[0].redes_estudiante}">Perfil en redes sociales</a></p>
+                                        </div>
+                                        <div class="contenido-info-u">
+                                            <p>Profesor</p>
+                                            <p>${project[0].nombre_docente}</p>
+                                            <p>${project[0].correo_docente}</p>
+                                            <p>${project[0].otros_docente}</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="contenido-mas-info">
+                                        <p>${project[0].especialidad}</p>
+                                        <p>${project[0].asignatura}</p>
+                                        <p>${project[0].curso}</p>
+                                        <p>${project[0].palabras_clave}</p>
+                                    </div>
+
+                                    <div class="contenido-descripcion">
+                                        <p>${project[0].descripcion}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <footer>
+                    <div class="footer-container">
+                        <ul class="footer-links">
+                            <li><a href="#">Inicio</a></li>
+                            <li><a href="https://esdmadrid.es/">Acerca de nosotros</a></li>
+                            <li><a href="https://esdmadrid.es/">Servicios</a></li>
+                            <li><a href="https://esdmadrid.es/">Contacto</a></li>
+                        </ul>
+                        <div class="footer-img">
+                            <img src="/assets/img/esd invertido.png">
+                        </div>
+                    </div>
+                </footer>
+                `;
+                document.body.innerHTML = estructura;
+        })
+    }
---
+++ app/main.js
@@ -0,0 +1,37 @@
+window.onload = () => {
+    let lista = document.querySelector("#usuarios");
+
+    let mostrarDetalle = (e) => {
+    window.open(`../detalle.html?id=${e.currentTarget.id}`, '_blank');
+}
+
+
+    fetch('assets/data/anuario.json')
+        .then(res => res.json())
+        .then(data => {
+            let projectNum = 0;
+            data.forEach((project, index) => {
+                if (project.titulo != undefined && project.imagenes != "" && project.imagenes != undefined) {
+                    let portada = project.imagenes.slice(0, 62);
+                    projectNum++;
+                    let item = `
+                            <article class="unid-cajon" id="${projectNum}">
+                                <img class="img-cajon" src="${portada}"/> 
+                                <h2 class="titulo-cajon">${project.titulo}</h2> 
+                                <div class="info-cajon">
+                                <p class="autor-cajon">${project.nombre_estudiante}</p>  
+                                </div>
+                            </article>`;
+                    lista.innerHTML += item;
+                    
+                }
+            });
+        })
+        .then(() => {
+            let projects = document.querySelectorAll(".unid-cajon");
+            projects.forEach((project) => {
+                project.addEventListener("click", mostrarDetalle, true);
+            });
+        })
+        .catch(error => console.error('Error loading JSON:', error));
+};
